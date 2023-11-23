47c349e3eb4d100415f0e29ea50447cbf211147e
Victor Gabriel <victor_tiss@outlook.com>
2023-11-23 12:41:51 -0300

    form-script add

--- form-script.js
+++ form-script.js
@@ -5,10 +5,10 @@ var count = 1;
 // Função para adicionar um novo comentário
 function addOpinion(name, lastname, age, email, opinion) {
     var newOpinion = { 
-        id: count++,
+        id: ++count,
         name: name,
         lastname: lastname,
-        age: age,
+        age: parseInt(age),
         email: email,
         opinion: opinion
     }
@@ -22,12 +22,12 @@ function addOpinion(name, lastname, age, email, opinion) {
 function deleteOpinion(opinionId) {
     var updatedFormList = formList.filter( function (opinion) {
         return opinion.id !== opinionId;
-    })
+    });
 
     if (updatedFormList.length < formList.length) {
         formList = updatedFormList;
         localStorage.setItem('formList', JSON.stringify(formList));
-        renderFormList;
+        renderFormList();
     } else {
         alert('Opinião não encontrada.');
     }
@@ -44,16 +44,35 @@ function renderFormList() {
     var formListElement = document.getElementById('list');
     formListElement.innerHTML = '';
 
+    // List
     formList.forEach( function (opinion) {
         var listItem = document.createElement('li');
         var content =
-            `<span class="form-name">${opinion.name}</span><br>` +
+            `<div class="opinion-header">
+                <span class="form-name">${opinion.name}</span>
+                <button type="submit" class="delete-button" onclick="deleteOpinion(${opinion.id})">X</button>  
+            </div>` +
             `<span class="form-email">${opinion.email}</span><br>` +
             `<span class="form-opinion">${opinion.opinion}</span>`;
 
         listItem.innerHTML = content;
         formListElement.appendChild(listItem);
     })
+
+    // Buttons display
+    var deleteButtonElement = document.getElementById('deleteAll-button');
+    var searchByIdElement = document.getElementById('search-id');
+    var searchByEmailElement = document.getElementById('search-email');
+
+    if(formList.length > 0) {
+        deleteButtonElement.style.display = 'block';
+        searchByIdElement.style.display = 'block';
+        searchByEmailElement.style.display = 'block';
+    } else {
+        deleteButtonElement.style.display = 'none';
+        searchByIdElement.style.display = 'none';
+        searchByEmailElement.style.display = 'none';
+    }
 }
 
 // Recuperar a lista de comentário do localStorage
@@ -78,9 +97,80 @@ document.getElementById('form-inputs').addEventListener('submit', function (even
         opinionInput.value
     )
 
-    nameInput.value = '';
-    lastnameInput.value = '';
-    ageInput.value = 16; // Idade mínima
-    emailInput.value = '';
-    opinionInput.value = '';
+    cleanFields();
 })
+
+// Função para limpar os campos do formulário
+function cleanFields() {
+    document.getElementById('name').value = '';
+    document.getElementById('lastname').value = '';
+    document.getElementById('age').value = null;
+    document.getElementById('email').value = '';
+    document.getElementById('opinion').value = '';
+}
+
+// Função para deletar todos os elementos
+function deleteAll() {
+    localStorage.clear();
+    document.getElementById('list').innerHTML = '';
+}
+
+// Event listener para procurar uma opinião por id
+document.getElementById('search-id').addEventListener('click', function() {
+    var id = prompt('Digite o ID que você deseja procurar');
+
+    if (id !== null) {
+        var found = false;
+
+        for (var i = 0; i < formList.length; i++) {
+            if (formList[i].id == id) {
+                alert(
+                    `Nome: ${formList[i].name}\n` +
+                    `Sobrenome: ${formList[i].lastname}\n` +
+                    `Idade: ${formList[i].age}\n` +
+                    `E-mail: ${formList[i].email}\n` +
+                    `Opinião: ${formList[i].opinion}\n`
+                    )
+                    found = true;
+                    break;
+            }
+        }
+        
+        if (!found) {
+            alert("ID não encontrado!");
+        }     
+    } else {
+        alert("Nenhum id inserido!");
+    }
+})
+
+// Event listener para procurar uma opinião por email
+document.getElementById('search-id').addEventListener('click', function() {
+    var id = prompt('Digite o ID que você deseja procurar');
+
+    if (id !== null) {
+        var found = false;
+
+        for (var i = 0; i < formList.length; i++) {
+            if (formList[i].id == id) {
+                alert(
+                    `Nome: ${formList[i].name}\n` +
+                    `Sobrenome: ${formList[i].lastname}\n` +
+                    `Idade: ${formList[i].age}\n` +
+                    `E-mail: ${formList[i].email}\n` +
+                    `Opinião: ${formList[i].opinion}\n`
+                    )
+                    found = true;
+                    break;
+            }
+        }
+        
+        if (!found) {
+            alert("ID não encontrado!");
+        }     
+    } else {
+        alert("Nenhum id inserido!");
+    }
+})
+
+
